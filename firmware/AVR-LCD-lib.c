57b22632f7e7029e70534dd00d78750ea70d9616
sandaz13 <mike@sanders-tech.com>
2013-08-08 22:41:34 -0500

    extracted working AVR-LCD code to AVR-LCD-lib
    extracted working code to an external library and cleaned up main.c to
    be a simple driver program.  Have not validated compilation.
    AVR-LCD-lib.h may not be in correct .h file format for the compiler
    we're using

--- firmware/AVR-LCD-lib.c
+++ firmware/AVR-LCD-lib.c
@@ -2,39 +2,36 @@
 #include <stdlib.h>
 #include <util/delay.h>
 #include <stdio.h>
+#include <string.h>
+#include "AVR-LCD-lib.h"
 
 //Define functions
 //==========================================================
+int lcd_out(unsigned char *string);
 void io_init(void);                         
 void lcd_init(void);
 void lcd_finish_init(void);
 void add_character(unsigned char position, unsigned char * letter);
 void send_nibble(unsigned char __rs, unsigned char __data);
-void send_string(unsigned char * string);
+void send_string(unsigned char string[]);
 void send_character(unsigned char position, unsigned char letter);
+
 //==========================================================
 
-int main(void) {	
+/*//  Wrapper function to initialize the LCD display and pass the string 
+////
+////
+*/
+int lcd_out(unsigned char *string){	
 	
-    io_init();
-	
-	lcd_init();
-	
-	/*// Smiley face. My own creation.
-	unsigned char smileyFace[16] = {0b0001, 0b1011, 0b0000, 0b0000, 
-		0b0000, 0b0100, 0b0000, 0b0100,
-		0b0001, 0b0001, 0b0001, 0b0001,
-		0b0001, 0b0001, 0b0001, 0b1111};
-	
-	add_character(0b00000000, &smileyFace[0]);*/
-	
+	// initialize the LCD screen - this should be generic for most LCD's using ##??## display type
+    io_init();	
+	lcd_init();	
 	lcd_finish_init();
 	
-	char string[15];
-	char * string = "squirrel man";
 	send_string(string);
 	
-	 return(0);
+	return(1);
 }
 	
 	
@@ -42,33 +39,29 @@ int main(void) {
 //// could use additional checks around the max size of the array, given a specific LCD
 //// 
 */
-void send_string(unsigned char * string){
-	int numChars = sizeof(string) / sizeof(string[0]));
-	
+void send_string(unsigned char* string){
+	short numChars = strlen(string);
 	short i;
 	short pos;
 	for(i = 0; i < numChars; i++) {
 			pos=i+1;
 			send_character(pos, string[i]);
-			
-			_delay_ms(150);
 	}
-
+}
 
 //  Sends a single character to the LCD at a given position
 //  could use checks to be sure not to breach the max size of the LCD
 //
+//  **NOTES: position currently unused - this may be useful to add in for more complex writes
+//
 void send_character(unsigned char position, unsigned char letter){
 	unsigned char halfChar = letter >> 4;
-	send_nibble(i,halfChar);
+	send_nibble(1,halfChar);
 	
 	halfChar = letter << 4;
 	halfChar = halfChar >> 4;
-	
-	send_nibble(i,halfChar);
+	send_nibble(1,halfChar);
 }
-	
-	
 
 
 void io_init (void) {
@@ -96,23 +89,6 @@ void lcd_init(void) {
 	send_nibble(0,0b1000);
 }
 
-void add_character(unsigned char position, unsigned char * letter) {
-	position = position | 0b01000000; // Ensure writing to CGRAM command
-	
-	unsigned char temp = position >> 4;
-	send_nibble(0,temp);
-	
-	temp = position << 4;
-	temp = temp >> 4;
-	
-	send_nibble(0,temp);
-	
-	// Write character.
-	short i;
-	for(i = 0; i < 16; i++) {
-		send_nibble(1, letter[i]);
-	}
-}
 
 void lcd_finish_init(void) {
 	send_nibble(0,0b0000);   //Display ON, Cursor Off, Cursor Not Blinking
@@ -130,4 +106,25 @@ void send_nibble(unsigned char __rs, unsigned char __data) {
 	_delay_ms(1);
 	PORTC = (__rs<<4) | __data;                   // Set RS & Data. Set EN=Low
 	_delay_ms(1);
+}
+
+//
+//  Unused - this is for custom characters, would need to extend the library to use it
+//
+void add_character(unsigned char position, unsigned char * letter) {
+	position = position | 0b01000000; // Ensure writing to CGRAM command
+	
+	unsigned char temp = position >> 4;
+	send_nibble(0,temp);
+	
+	temp = position << 4;
+	temp = temp >> 4;
+	
+	send_nibble(0,temp);
+	
+	// Write character.
+	short i;
+	for(i = 0; i < 16; i++) {
+		send_nibble(1, letter[i]);
+	}
 }
